21b1adb39b99ddacd1c4a0f8016240eae3e9d782
PrakharGoenka <prakhar.goinka@gmail.com>
2020-11-22 18:50:43 +0530

    Implement the VertexRecord class

--- VertexRecord.h
+++ VertexRecord.h
@@ -1,1 +1,22 @@
-/* Stores Coordinates, References to an incident edge. */
+#ifndef VERTEXRECORD_H
+#define VERTEXRECORD_H
+
+#include <bits/stdc++.h>
+#include "Point.h"
+#include "EdgeRecord.h"
+
+class VertexRecord {
+public:
+   Point coordinates;
+   EdgeRecord incident_edge;
+
+   void updateCoordinates(pair <double, double> point_coordinates) {
+       coordinates = Point(point_coordinates);
+   }
+
+   void updateIncidentEdge(EdgeRecord edge) {
+	   incident_edge = edge;
+   }
+};
+
+#endif /*VERTEXRECORD_H*/
