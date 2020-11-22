8b60824fc1e29399f9d425a680da1b45de0b0e3d
PrakharGoenka <prakhar.goinka@gmail.com>
2020-11-22 18:51:06 +0530

    Implement the EdgeRecord class

--- EdgeRecord.h
+++ EdgeRecord.h
@@ -1,2 +1,36 @@
-/* Reference to origin vertex record, reference to the incident face,
-references to the twin, next, and previous half edges */
+#ifndef EDGERECORD_H
+#define EDGERECORD_H
+
+#include "VertexRecord.h"
+#include "FaceRecord.h"
+
+class EdgeRecord {
+public:
+	VertexRecord origin;
+	FaceRecord incident_face;
+	EdgeRecord* twin;
+	EdgeRecord* next;
+	EdgeRecord* previous;
+
+	void addOrigin(VertexRecord& v) {
+		origin = v;
+	}
+
+	void updateFace(FaceRecord f) {
+		incident_face = f;
+	}
+
+	void updateTwin(EdgeRecord& t) {
+		twin = &t;
+	}
+
+	void updateNext(EdgeRecord& n) {
+		twin = &n;
+	}
+
+	void updatePrevious(EdgeRecord& p) {
+		twin = &p;
+	}
+};
+
+#endif /*EDGERECORD_H*/
