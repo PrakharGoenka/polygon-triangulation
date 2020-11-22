a3d52ba4a99d254ebcd48b9e386943f1eab95773
PrakharGoenka <prakhar.goinka@gmail.com>
2020-11-22 16:39:29 +0530

    Implement the Point class

--- Point.h
+++ Point.h
@@ -1,1 +1,26 @@
-/* Abstracts a point in 2-D space */
+#ifndef POINT_H
+#define POINT_H
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*! Class to abstract a point in the 2D space.
+*/
+class Point {
+  public:
+  pair <double, double> coordinates;
+
+  /*! Constructor to initialize the Point object.
+  * \param c - A pair of doubles. It stores the coordinates of the point. 
+  */
+  Point(pair <double, double> c) {
+    coordinates = c;
+  }
+
+  bool operator< (const Point& p) const {
+    return this->coordinates < p.coordinates;
+  }
+};
+
+#endif /*POINT_H*/
