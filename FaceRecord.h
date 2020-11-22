24fe1a865426f09b7368aa07a5affbf1aac79a9c
PrakharGoenka <prakhar.goinka@gmail.com>
2020-11-22 18:51:26 +0530

    Implement the FaceRecord class.

--- FaceRecord.h
+++ FaceRecord.h
@@ -1,1 +1,20 @@
-/* Abstracts a face record */
+#ifndef FACERECORD_H
+#define FACERECORD_H
+
+#include "EdgeRecord.h"
+
+class FaceRecord {
+public:
+    EdgeRecord inner_component;
+    EdgeRecord outer_component;
+
+    void updateInnerComponent(EdgeRecord edge) {
+        inner_component = edge;
+    }
+
+    void updateOuterComponent(EdgeRecord edge) {
+        outer_component = edge;
+    }
+};
+
+#endif /*FACERECORD_H*/
