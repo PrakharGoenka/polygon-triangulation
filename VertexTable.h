b31b64028c91a633db2220e2c23432c0a71f59db
PrakharGoenka <prakhar.goinka@gmail.com>
2020-11-22 21:38:38 +0530

    Implement the VertexTable class

--- VertexTable.h
+++ VertexTable.h
@@ -1,4 +1,32 @@
-/* Data: Maps of records (index -> record reference)
-Will contain methods to update own table instances,
-for example, adding a record, updating / deleting records.
-*/
+#ifndef VERTEXTABLE_H
+#define VERTEXTABLE_H
+
+#include <bits/stdc++.h>
+#include "VertexRecord.h"
+
+
+class VertexTable {
+public:
+    vector <VertexRecord> table;
+
+    int addVertexRecord(pair <double, double> coordinates) {
+        VertexRecord v(table.size());
+        v.updateCoordinates(coordinates);
+        table.push_back(v);
+        return v.index;
+    }
+
+    void updateIncidentEdge(int index, int edge) {
+        table[index].updateIncidentEdge(edge);
+    }
+
+    pair <double, double> getVertexCoordinates(int index) {
+        return table[index].vertex.coordinates;
+    }
+
+    int getIncidentEdge(int index) {
+        return table[index].incident_edge;
+    }
+};
+
+#endif /*VERTEXTABLE_H*/
