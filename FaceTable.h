c4eb14b03c2fb60697d31ce2efc43544485f6167
PrakharGoenka <prakhar.goinka@gmail.com>
2020-11-22 21:57:19 +0530

    Implement the FaceTable class

--- FaceTable.h
+++ FaceTable.h
@@ -1,4 +1,35 @@
-/* Data: Maps of records (index -> record reference)
-Will contain methods to update own table instances,
-for example, adding a record, updating / deleting records.
-*/
+#ifndef FACETABLE_H
+#define FACETABLE_H
+
+#include <bits/stdc++.h>
+#include "FaceRecord.h"
+
+
+class FaceTable {
+public:
+    vector<FaceRecord> table;
+
+    int addFaceRecord() {
+        FaceRecord f(table.size());
+        table.push_back(f);
+        return f.index;
+    }
+
+    void updateInnerComponent(int index, int edge) {
+        table[index].inner_component = edge;
+    }
+
+    void updateOuterComponent(int index, int edge) {
+        table[index].outer_component = edge;
+    }
+
+    int getInnerComponent(int index) {
+        return table[index].inner_component;
+    }
+
+    int getOuterComponent(int index) {
+        return table[index].outer_component;
+    }
+};
+
+#endif /*FACETABLE_H*/
